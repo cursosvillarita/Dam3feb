f6c62ca156a7a2fe0c2d11ced7521fe60de68bb6
ETS <ETS@172.32.3.100>
2017-02-03 11:33:47 +0000

    El primero

---
+++ src/operaciones.java
@@ -0,0 +1,20 @@
+public class operaciones {
+	private int a;
+	private int b;
+	public  operaciones (int a, int b) {
+	this.a = a;
+	this.b = b;
+	}
+	public int suma() {
+	return a + b;
+	}
+	public int resta() {
+	return a - b;
+	}
+	public double división() {
+	return (double)a / b;
+	}
+	public int multiplicación() {
+	return a * b;
+}
+}
